2ec6ad6b8420d24d77ba92a385a5230ed689830a
Nhlanhla Mbokazi <nhlanhla.mbokazi@dadevs.co.za>
2023-08-16 20:45:28 +0200

    Added slideshow to the home component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -5,7 +5,17 @@ import HomePageText from '@/assets/HomePageText.png';
 import HomePageGraphic from '@/assets/Logo.jpg';
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
+const images = [
+    'src/assets/real-estate-374190_1280.jpg',
+    'src/assets/Image1.jpg',
+    'src/assets/Image2.jpg',
+    'src/assets/Image3.jpg',
+    'src/assets/Image4.jpg',
+    'src/assets/exit-419280_1280.jpg',
+    
+  ];
 
 
 type Props = {
@@ -16,68 +26,41 @@ const Home = ({ setSelectedPage}: Props) => {
 
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const nextImage = () => {
+        setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(nextImage, 5000); // Change image every 5 seconds
+        return () => clearInterval(interval);
+      }, []);
+
   return (
     <section
         id="home"
-        className="gap-16 mt-2 py-10 bg-cover bg-center bg-no-repeat h-screen md:h-full bg-transparent md:pd-0 bg-[url('@/assets/real-estate-374190_1280.jpg')] w-11/12 mx-auto"
+        className="h-screen relative overflow-hidden w-11/12 mx-auto"
     >
-        {/* Image and main header */}
-        <motion.div 
-            className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
-            onViewportEnter={ () => setSelectedPage(SelectedPage.Home)}
-        >
-
-            {/* Main Header */}
-
-            <div className="  md:basis-3/5">
-
-                {/* Headings */}
-                <motion.div 
-                className="md:mt-20"
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.5}}
-                transition={{ duration: 0.5 }}
-                variants={{
-                    hidden: { opacity: 0, x: -50},
-                    visible: { opacity: 1, x: 0}
-
-                }}
-                >
-                  
-                </motion.div>
-
-                      
-            </div>
-
-           
-
-                
-
-        </motion.div>
-
-        
-         {/* Actions */}
-         <motion.div 
-                className="mt-8 justify-center flex items-center gap-8 "
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.5}}
-                transition={{ delay:0.2, duration: 0.5 }}
-                variants={{
-                    hidden: { opacity: 0, x: -50},
-                    visible: { opacity: 1, x: 0}
-
-                }}
-                >
-                    <ActionButton setSelectedPage={setSelectedPage}>
-                        Join Now
-                    </ActionButton>
-                   
-
-                    
-                </motion.div> 
-        
+      
+      <motion.img
+        src={images[currentImage]}
+        alt="Slideshow"
+        className="object-cover h-full w-full absolute inset-0"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        key={currentImage}
+      />
+      <div className="absolute inset-0 flex justify-center items-center">
+        <div className="bg-white p-4 rounded-lg shadow-md">
+          <h1 className="text-3xl font-semibold mb-4">Welcome to Our Website</h1>
+          <p className="text-gray-600">
+            Discover the best door repair and installation services in town.
+          </p>
+        </div>
+      </div>
+      
                 
     </section>
 
